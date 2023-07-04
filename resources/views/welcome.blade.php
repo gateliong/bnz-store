40326d250c5e1316d1e3e0315c4db8e0b1392a00
gateliong <bnzkidss@gmail.com>
2023-07-04 09:50:58 +0700

    first order

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="cache-control" content="max-age=0"/>
+    <meta http-equiv="cache-control" content="no-cache"/>
+    <meta http-equiv="expires" content="0"/>
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT"/>
+    <meta http-equiv="pragma" content="no-cache"/>
+    <meta charset="UTF-8">
+    <meta name=viewport content="user-scalable=no,initial-scale=1,minimum-scale=1,maximum-scale=1,width=device-width">
+    <meta name=apple-mobile-web-app-capable content=yes>
+    <title>BnZ Store | good fashion</title>
+    <meta name=description content="ACME Intralog is a leading Industrial and Warehouse Automation Company in Dubai, the UAE. Warehouse Automation Solutions not just reduce your manual work but also increase your productivity.">
+    <meta property=og:site_name content="Acme World">
+    <meta property=og:title content="Industrial & Warehouse Automations Company in Dubai, UAE | ACME Intralog">
+    <meta property=og:description content="">
+    <meta property=og:url content="https://BnZ-Store.com/">
+    <meta property=og:type content=website>
+    <meta property=og:image content="https://BnZ-Store/icons.jpg">
+    <meta property=og:locale content=en_EN>
+    <meta name=twitter:card content=summary_large_image>
+    <meta name=twitter:site content=@Bnz-Store>
+    <meta name=twitter:creator content="">
+    <meta name=twitter:title content="Bnz Store & Warehouse Fashion | Bnz Store">
+    <meta name=twitter:text:description content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    
+</body>
+</html>
